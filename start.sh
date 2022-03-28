524096362dc3cbd2261d662a4523cf9bfdd2de23
Anandsreenivasan <anand.sreenivasan@hotmail.com>
2022-03-28 08:31:32 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-chmod +x ./start-process.sh
-nohup ./start-process.sh > foo.out 2> foo.err < /dev/null &
+chmod +x /home/ec2-user/start-process.sh
+nohup /home/ec2-user/start-process.sh > foo.out 2> foo.err < /dev/null &
